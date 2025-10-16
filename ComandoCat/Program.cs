a92542e008aeff541200fcb1491911d155e719ed
carlos <carlos.paz.miranda@gmail.com>
2025-10-16 12:09:02 +0200

    Empezado el newfile

--- ComandoCat/Program.cs
+++ ComandoCat/Program.cs
@@ -16,37 +16,60 @@ namespace ComandoCat
                 Console.WriteLine("No se ha pasado ningun argumento");
                 return 2;
             }
-            string fichero = Environment.ExpandEnvironmentVariables(args[0]);
-            if (!File.Exists(fichero))
-            {
-                Console.WriteLine("El fichero no existe");
-                return 1;
-            }
-            int numLineas = 0;
+
+            string fichero;
+            int numLineas = -1;
             if (args[0].StartsWith("-n"))
             {
                 string numFilas = args[0].Substring(2);
-                if (!int.TryParse(numFilas, out  numLineas) || numLineas <= 0)
+                if (!int.TryParse(numFilas, out numLineas) || numLineas <= 0)
                 {
                     Console.WriteLine("El argumento no es valido");
                     return 2;
                 }
+
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("No se ha especificado el fichero");
+                    return 2;
+                }
+
+                fichero = Environment.ExpandEnvironmentVariables(args[1]);
+                if (!File.Exists(fichero))
+                {
+                    Console.WriteLine("El fichero no existe");
+                    return 1;
+                }
+            }
+            else
+            {
+                fichero = Environment.ExpandEnvironmentVariables(args[0]);
             }
             try
             {
                 using (StreamReader sr = new StreamReader(fichero))
                 {
-
                     string linea;
+                    int contador = 0;
+
                     while ((linea = sr.ReadLine()) != null)
                     {
                         Console.WriteLine(linea);
+                        contador++;
+
+                        if (numLineas > 0 && contador >= numLineas)
+                            break;
                     }
                 }
             }
             catch (UnauthorizedAccessException ex)
             {
                 Console.WriteLine("No tienes permisos para acceder a este fichero");
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al leer el fichero: " + ex.Message);
                 return -1;
             }
             return 0;
---
+++ ComandoNewFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandoNewFile
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No se ha pasado ningun argumento");
+                return 2;
+            }
+            if (args[0].StartsWith("-a"))
+            {
+                
+            }
+            return 0;
+        }
+    }
+}
